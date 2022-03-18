e82dfe1b500b1c10b334734bb3492aad9c4d54af
Blanca <blancacano2.71828@gmail.com>
2022-03-18 16:18:58 +0100

    Implementa 1-NN falta test #3

--- scripts/julia_pkg_instalations.jl
+++ scripts/julia_pkg_instalations.jl
@@ -1,3 +1,6 @@
 import Pkg
+# Para leer ficheros
 Pkg.add("ARFFFiles")
 Pkg.add("DataFrames")
+# Para tener el valor infinito
+Pkg.add("Infinity")
---
+++ src/one-NN.jl
@@ -0,0 +1,24 @@
+module ModuloOneNN
+export OneNN(x, dist, data, labels)
+
+using Infinity
+"""
+    One-NN(x, dist, data, labels)
+    Devuelve la clase a la que clasifica $x$ 
+    a partir del vecino más cercano dentro del conjunto de datos.
+"""
+function OneNN(x, dist, data, labels)
+    distanciaMinima = ∞
+    clase = nothing 
+    index = 1
+    for elemen in eachrow(data)
+        d = dist(element, x)
+        if d < distanciaMinima
+            distanciaMinima = d 
+            clase = labels[index]
+        end
+        index += 1
+    end
+    return clase 
+end
+end #ModuloOneNN
