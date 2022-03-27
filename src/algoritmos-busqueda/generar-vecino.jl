05f87fee9a858f29695820240fea210f3178ced8
Blanca <blancacano2.71828@gmail.com>
2022-03-27 17:10:39 +0200

    Implementa algoritmo de búsqueda local #6 Falta añadirlo a un código general

--- src/algoritmos-busqueda/generar-vecino.jl
+++ src/algoritmos-busqueda/generar-vecino.jl
@@ -1,7 +1,7 @@
 export GenNeighbourhood
+export GenNeighbourhoodIesimo
 
-using Random, Distributions
-#Random.seed!(123) # Setting the seed
+using Distributions
 
 """
     GenNeighbourhood(w::Vector{Real} ,µ::Real, var ::Real)
@@ -15,5 +15,19 @@ function GenNeighbourhood(w::Vector{<:Real} ,µ::Real, var ::Real)::Vector{<:Rea
         x -> min(1, max(0,x)),
         w-z
     )
+    return w_new
+end
+
+
+"""
+GenNeighbourhoodIesimo(w::Vector{<:Real} ,µ::Real, var ::Real, index::Int)::Vector{<:Real}
+    Devuelve vecino del vector `w` de acorde a una normal 
+    de media `µ` y varianza `var` donde solo se ha cambia la componente iéxima. 
+"""
+function GenNeighbourhoodIesimo(w::Vector{<:Real} ,µ::Real, var ::Real, index::Int)::Vector{<:Real}
+    distribution = Normal(µ, var)
+    z=rand(distribution)
+    w_new = w
+    w_new[index] = min(1, max(0,w_new[index]-z))
     return w_new
 end
--- src/test/generar-vecinos.test.jl
+++ src/test/generar-vecinos.test.jl
@@ -1,15 +1,38 @@
 using Test 
 include("../algoritmos-busqueda/generar-vecino.jl")
+include("../algoritmos-busqueda/busqueda-local.jl")
 
-@testset "Buscar vecino" begin 
+@testset "Buscar vecino cambiando todo " begin 
     w = [0.5, 0.5, 0.5]
     m = 0
     s = 0.3
-w_n = GenNeighbourhood(w, m, s)
-    for i in 1:5
+    w_n = GenNeighbourhood(w, m, s)
+    for i in 1:3
         @test size(w_n) == size(w)
         @test !(false in map( x-> 0<= x <= 1, w_n))
         w = w_n
         GenNeighbourhood(w, m, s)
+    end
+end
+@testset "Buscar vecino un índice concreto" begin 
+    for i in 1:5
+        w = [0.5, 0.5, 0.5, 0.5, 0.5]
+        m = 0
+        s = 0.3
+        w_n = GenNeighbourhoodIesimo(w, m, s, i)
+        @test size(w_n) == size(w)
+        @test !(false in map( x-> 0<= x <= 1, w_n))
+        @test w[1:i-1] == w_n[1:i-1]
+        @test w[i+1:5] == w_n[i+1:5]
+    end
+end
+
+@testset "Primero mejor" begin 
+    F(x)=sum(x) # Función de evaluación simple
+    
+    for tam in 12:15
+        w = PrimeroMejor(10, 2, tam, 0.3, F)
+        @test length(w) == tam
+        @test !(false in map( x-> 0<= x <= 1, w))
     end
 end
